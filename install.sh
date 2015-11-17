8e426cc84fd38cfd73e24830050e32660df2ae35
ImoucheG <imouche.guillaume@gmail.com>
2015-11-17 08:50:00 +0100

    init

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
-Script utilisé sous Debian 8.
+#Script utilisé sous Debian 8.
 
-# On met à jour le poste#
-apt-get update -y && apt-get upgrade -y
+# On met a jour le poste#
+#apt-get update -y && apt-get upgrade -y
 
 # On installe MySqlServer #
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password 3p$!’
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password 3p$!’
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password 3p$!'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password 3p$!'
 apt-get install -y mysql-server
 # On installe apache 2 #
 apt-get install -y apache2
-# On installe PHP avec ses dépendances # 
+# On installe PHP avec ses dependances # 
 apt-get  install -y php5 libapache2-mod-php5
-# On redémarre le serveur apache #
+# On redemarre le serveur apache #
 service apache2 restart
