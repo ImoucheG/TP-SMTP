8f6ce2a369db0293af939d5c43140c95694aa80a
ImoucheG <imouche.guillaume@gmail.com>
2015-11-19 22:54:11 +0100

    fd

---
+++ setup_SMTP.sh
@@ -0,0 +1,15 @@
+## Setup SMTP Server ##
+## Update System ##
+apt-get update -y && apt-get upgrade -y
+apt-get install sudo
+## Install MysqlServer ##
+su epsi
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password 3p$!"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password 3p$!"
+sudo apt-get install -y mysql-server
+sudo mysql_install_db
+## Install Nginx ##
+sudo apt-get install -y nginx
+sudo apt-get install -y php5-fpm php5-mysql
+service nginx restart
+## DNS ## 
