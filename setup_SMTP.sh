f69d0f06cc7f2831f0767186e1bd94c7a8ebadb3
root <root@giraone.gira-labos-nantes.ovh>
2015-11-19 15:46:48 -0600

    a

--- setup_SMTP.sh
+++ setup_SMTP.sh
@@ -1,17 +1,16 @@
 ## Setup SMTP Server ##
 ## Update System ##
-apt-get update -y && apt-get upgrade -y
+#apt-get update -y && apt-get upgrade -y
 apt-get install sudo
 adduser epsi sudo
 adduser epsi root
 ## Install MysqlServer ##
-su epsi
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password 3p$!"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password 3p$!"
-sudo apt-get install -y mysql-server
-sudo mysql_install_db
+debconf-set-selections <<< "mysql-server mysql-server/root_password password 3p$!"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password 3p$!"
+apt-get install -y mysql-server
+mysql_install_db
 ## Install Nginx ##
-sudo apt-get install -y nginx
-sudo apt-get install -y php5-fpm php5-mysql
+apt-get install -y nginx
+apt-get install -y php5-fpm php5-mysql
 service nginx restart
-## DNS ## 
+## DNS ##
