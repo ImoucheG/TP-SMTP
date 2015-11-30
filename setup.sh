b10ad7f80e80ae41067ce624afc984fd8c7ace95
ImoucheG <imouche.guillaume@gmail.com>
2015-11-30 22:10:04 +0100

    Install Mysql Php Nginx and user add epsi with group root and sudo

--- setup.sh
+++ setup.sh
@@ -1,16 +1,20 @@
-## Setup SMTP Server ##
-## Update System ##
-#apt-get update -y && apt-get upgrade -y
-apt-get install sudo
-adduser epsi sudo
+# Setup SMTP Server
+# Update System
+apt-get update -y && apt-get upgrade -y
+apt-get install -y sudo
+# Add user EPSI
+useradd epsi
 adduser epsi root
-## Install MysqlServer ##
-debconf-set-selections <<< "mysql-server mysql-server/root_password password 3p$!"
-debconf-set-selections <<< "mysql-server mysql-server/root_password_again password 3p$!"
+adduser epsi sudo
+# Install MysqlServer
+echo 'mysql-server mysql-server/root_password password 3p$!' | debconf-set-selections
+echo 'mysql-server mysql-server/root_password_again password 3p$!' | debconf-set-selections
 apt-get install -y mysql-server
 mysql_install_db
-## Install Nginx ##
+# Install Nginx
 apt-get install -y nginx
+# Install PHP
 apt-get install -y php5-fpm php5-mysql
 service nginx restart
-## DNS ##
+#Domain
+
