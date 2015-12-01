c83177013abda7b29b786ce09e41b7c81332a964
ImoucheG <imouche.guillaume@gmail.com>
2015-12-01 10:08:19 +0100

    Configure DNS with labos-nantes.ovh, the hostname is gira

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # Setup SMTP Server
 # Update System
-apt-get update -y && apt-get upgrade -y
+#apt-get update -y && apt-get upgrade -y
 apt-get install -y sudo
 # Add user EPSI
 useradd epsi
@@ -16,5 +16,16 @@ apt-get install -y nginx
 # Install PHP
 apt-get install -y php5-fpm php5-mysql
 service nginx restart
-#Domain
-
+# Install Domain
+echo "\ngira.labos-nantes.ovh" >> /etc/hostname
+/etc/init.d/hostname.sh start
+echo "\n127.0.0.1 localhost.labos-nantes.ovh localhost" >> /etc/hosts
+echo "\n192.168.56.101 gira.labos-nantes.ovh gira" >> /etc/hosts
+echo "\norder hosts, bind" >> /etc/hosts.conf
+echo "\nmulti on" >> /etc/hosts.conf
+echo "\ndomain labos-nantes.ovh" >> /etc/resolv.conf
+echo "\nsearch labos-nantes.ovh" >> /etc/resolv.conf
+echo "\nnameserver 192.168.56.101" >> /etc/resolv.conf
+apt-get install -y bind9 dnsutils
+cp -R /media/sf_VMShared/TP-SMTP/config_bind9/* /etc/bind
+service bind9 restart
