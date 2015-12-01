f347ad229587a02108d2b9e51b7e06fdd980e13f
ImoucheG <imouche.guillaume@gmail.com>
2015-12-01 12:35:50 +0100

    Begining install postfix

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ useradd epsi
 adduser epsi root
 adduser epsi sudo
 # Install MysqlServer
-echo 'mysql-server mysql-server/root_password password 3p$!' | debconf-set-selections
-echo 'mysql-server mysql-server/root_password_again password 3p$!' | debconf-set-selections
+echo 'mysql-server mysql-server/root_password password mysql' | debconf-set-selections
+echo 'mysql-server mysql-server/root_password_again password mysql' | debconf-set-selections
 apt-get install -y mysql-server
 mysql_install_db
 # Install Nginx
@@ -29,3 +29,17 @@ echo "\nnameserver 192.168.56.101" >> /etc/resolv.conf
 apt-get install -y bind9 dnsutils
 cp -R /media/sf_VMShared/TP-SMTP/config_bind9/* /etc/bind
 service bind9 restart
+mysql -u root -pmysql -e "CREATE database postfix;"
+mysql -u root -pmysql -e "CREATE USER 'postfix'@'localhost' IDENTIFIED BY 'postfix';"
+mysql -u root -pmysql -e "GRANT USAGE ON *.* TO 'postfix'@'localhost';"
+mysql -u root -pmysql -e "GRANT ALL PRIVILEGES ON postfix.* TO 'postfix'@'localhost';"
+cd /var/www
+wget http://imoucheg.com/postfixadmin-2.93.tar.gz
+tar -xzf postfixadmin-2.93.tar.gz
+mv postfixadmin-2.93 postfixadmin
+rm -rf postfixadmin-2.93.tar.gz
+chown -R www-data:www-data postfixadmin
+apt-get install -y php5-imap
+cp /media/sf_VMShared/TP-SMTP/config_postfixadmin/postfixadmin.conf /etc/nginx/sites-enabled/
+cp /media/sf_VMShared/TP-SMTP/config_postfixadmin/config.inc.php /var/www/postfixadmin/
+service nginx restart
