e73ee37515cd0bf9a95dff863c6e4e04ba50989a
Arthur Rousseau <arthur.rousseau2@gmail.com>
2015-12-07 22:41:14 +0100

    Externalisation des configurations systèmes Passage en script des commandes "longues" (mysql, echo...)

---
+++ config/postfix/setup.sql
@@ -0,0 +1,4 @@
+CREATE database postfix;
+CREATE USER 'postfix'@'localhost' IDENTIFIED BY 'postfix';
+GRANT USAGE ON *.* TO 'postfix'@'localhost';"
+GRANT ALL PRIVILEGES ON postfix.* TO 'postfix'@'localhost';
