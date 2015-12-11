76c380b1ef494164043346f0f94f4e8bc09222b1
Arthur Rousseau <arthur.rousseau2@gmail.com>
2015-12-11 01:27:10 +0100

    Fixed hostname, readme init

--- README.md
+++ README.md
@@ -1,4 +1,43 @@
-# TP-SMTP
-"""
-sh setup.sh
-"""
+# Installation d’un serveur de messagerie
+
+## Définition du besoin
+Votre client veut voir mis en place un serveur de messagerie sécurisé. Pour des raisons de coûts, la distribution Debian a été choisie.
+L’entreprise souhaite de votre part un prototype sur machine virtuelle.
+
+## Contraintes
+Pour s’intégrer dans son environnement, l’entreprise a choisi de mettre en place un serveur de messagerie avec les éléments suivants :
+* [Postfix] comme serveur SMTP. Postfix est Open Source, rapide, sécurisé, fiable et sa 
+configuration de base est abordable.
+* [Postfixadmin] pour l'administration de notre seveur SMTP.
+* [Dovecot] comme service de réception (POP3, POP3S, IMAP, IMAPS). La configuration de Dovecot est assez simple et Postfix s'appuiera sur Dovecot ou sur SASL pour le mécanisme d'authentification.
+* Authentification SMTP pouvoir utiliser le serveur depuis l'extérieur du LAN.
+* Envois d'emails en SMTPS, c'est à dire que les emails seront cryptés entre le poste de  travail et le serveur. C'est nécessaire si on utilise un ordinateur portable sur un réseau non sécurisé (hot spot wifi, etc.).
+* Réception des emails en IMAPS et/ou POP3S, c'est à dire que les données seront cryptées entre le serveur et le poste de travail.
+* Utilisation d'un annuaire LDAP pour la vérification des mots de passe.
+* Protection contre les spams et les virus.
+* Utilisation d'un webmail pour consulter les emails depuis une interface web.
+
+## Documentation
+La documentation complète, rédigée et mise en forme sera à rendre sous forme électronique.
+Les différents fichiers de configuration sont attendu réunis dans une archive : paramétrage des services, adresse IP, comptes et mot de passe...
+
+### Version
+1.0.0
+
+### Installation
+```sh
+$ cd /tmp/
+$ git clone https://github.com/ImoucheG/TP-SMTP.git
+$ cd TP-SMTP
+$ chmod a+x setup.sh && ./setup.sh
+```
+
+### Todos
+Coming soon.
+
+### License
+Apache
+
+[postfix]: http://www.postfix.org/
+[postfixadmin]: http://postfixadmin.sourceforge.net/
+[dovecot]: http://www.dovecot.org/
--- setup.sh
+++ setup.sh
@@ -11,20 +11,7 @@ useradd epsi
 adduser epsi root
 adduser epsi sudo
 
-# Install MySQL Server
-echo 'mysql-server mysql-server/root_password password mysql' | debconf-set-selections
-echo 'mysql-server mysql-server/root_password_again password mysql' | debconf-set-selections
-apt-get install -y mysql-server
-mysql_install_db
-
-# Install Apache
-apt-get install -y apache2
-
-# Install PHP
-apt-get install -y php5 libapache2-mod-php5 php5-fpm php5-mysql php5-imap
-service apache2 restart
-
-# Install Domain
+# BIND Installation
 apt-get install resolvconf -y
 cp ./config/misc/base /etc/resolvconf/resolv.conf.d/base
 resolvconf -u
@@ -35,15 +22,6 @@ cp ./config/misc/hostname /etc/hostname
 cp ./config/misc/hosts /etc/hosts
 cp ./config/misc/hosts.conf /etc/hosts.conf
 
-<<<<<<< HEAD
-cat ./config/misc/hosts >> /etc/hosts
-cat ./config/misc/hosts.conf >> /etc/hosts.conf
-cat ./config/misc/resolv.conf >> /etc/resolv.conf
-cp ./config/misc/interfaces /etc/network/interfaces
-ifdown eth0 eth1
-ifup eth0 eth1
-=======
->>>>>>> dev_arthur
 apt-get install -y bind9 dnsutils
 cp -R ./config/bind9/* /etc/bind/
 /etc/init.d/named restart
@@ -53,43 +31,50 @@ cp ./config/misc/interfaces /etc/network/interfaces
 ifdown eth0 eth1
 ifup eth0 eth1
 
-# Install PostfixAdmin
-#login : admin@labos-nantes.ovh
-#password : admin2015
-# LOCAL ONLY & DOMAIN : hostone.gira.labos-nantes.ovh 
-apt-get install -y postfix postfix-mysql
-mysql -u root -pmysql < ./config/postfix/setup.sql
-cd /var/www/
+# MySQL Server Installation
+echo 'mysql-server mysql-server/root_password password mysql' | debconf-set-selections
+echo 'mysql-server mysql-server/root_password_again password mysql' | debconf-set-selections
+apt-get install -y mysql-server
+mysql_install_db
 
-wget http://sourceforge.net/projects/postfixadmin/files/postfixadmin/postfixadmin-2.93/postfixadmin-2.93.tar.gz
-tar -xzf postfixadmin-2.93.tar.gz
-mv postfixadmin-2.93 postfixadmin
-rm -rf postfixadmin-2.93.tar.gz
-chown -R root:www-data postfixadmin
-chmod -R 775 postfixadmin
-cd /tmp/
-# Configuration PostfixAdmin #
-service apache2 restart
-# VirtualHost APACHE 2.0 #
-# Listen 8080 #
+# Apache Installation
+apt-get install -y apache2
 cat ./config/apache2/ports.conf >> /etc/apache2/ports.conf
 cat ./config/apache2/000-default.conf >> /etc/apache2/sites-enabled/000-default.conf
 
-# Restart Apache #
-service apache2 restart
-# Password postfixadmin: postfix2015
-# User : g.imouche@gira.labos-nantes.ovh::gimouche2015 // a.rousseau@gira.labos-nantes.ovh::arousseau2015
+# PHP Installation
+apt-get install -y php5 libapache2-mod-php5 php5-fpm php5-mysql php5-imap
 
+# Postfix Installation ('Local Only')
+# Login : admin@labos-nantes.ovh (should be : admin@gira.labos-nantes.ovh ?)
+# Password : admin2015
+# Domain : hostone.gira.labos-nantes.ovh (should be : gira.labos-nantes.ovh ?)
+apt-get install -y postfix postfix-mysql
+wget http://sourceforge.net/projects/postfixadmin/files/postfixadmin/postfixadmin-2.93/postfixadmin-2.93.tar.gz
+tar -xzf postfixadmin-2.93.tar.gz
+mv postfixadmin-2.93 /var/www/postfixadmin
+chown -R root:www-data /var/www/postfixadmin
+chmod -R 775 /var/www/postfixadmin
+mysql -u root -pmysql < ./config/postfix/setup.sql
+
+# Password postfixadmin: postfix2015
+# Users : 
+#	- g.imouche@gira.labos-nantes.ovh::gimouche2015
+#	- a.rousseau@gira.labos-nantes.ovh::arousseau2015
 cp ./config/postfixadmin/config.inc.php /var/www/postfixadmin/
-mysql -u root -pmysql postfix < ./config/postfixadmin/postfix.sql
 cp /etc/postfix/main.cf /etc/postfix/main.cf_bak
 cp -R ./config/postfix/* /etc/postfix/
+mysql -u root -pmysql postfix < ./config/postfixadmin/postfix.sql
+
 # Generation SSL #
 cd /tmp/config/ssl/
 mv hostone.gira.labos-nantes.ovh.key /etc/ssl/private/
 mv hostone.gira.labos-nantes.ovh.crt /etc/ssl/certs/
 mv cakey.pem /etc/ssl/private/
 mv cacert.pem /etc/ssl/certs/
+
+service apache2 restart
+
 # A mettre au propre avec une bonne config
 #Installation DOVECOT
 cd /tmp/
